d1a63e88fbe64efc7c1f73c4645ac76c67b551bc
AstroSnail <astrosnail@derpymail.org>
2017-07-25 09:13:10 +0100

    por coisas em sitios

--- interface.py
+++ interface.py
@@ -77,16 +77,26 @@ def do_booking():
     
 book_seats = PushButton(app, command=do_booking, text="Info", grid=[3,2], align="left") 
 def do_instrucoes():
-    info("instrucões","Press middle to (.)\
-                       Press down to (-)\
-                       Press right to add space\
-                       Press left to select a letter\
+    info("instructions-Morse","Press left to (.)\
+                            Press rigth to (-)\
+                       Press down to add space\
+                       Press middle to select a letter\
                        Press up to show the message\
-                       Press up for over 2 sec to make raspberry speak\
-                       Press right for over 2 sec to send email\
+                       Press left for over 2 sec to make raspberry speak\
+                       Press rigth for over 2 sec to send email\
                        \
-                       Press down for over 2 sec to go back to the menu")
+                       Press down for over 2 sec to go back to the menu\
+                       Press up for over 2 sec to clean the message")
 
-book_seats = PushButton(app, command=do_instrucoes, text="Instruções", grid=[3,3], align="left")
+info_1 = PushButton(app, command=do_instrucoes, text="Morse Instructions", grid=[3,3], align="left")
+def do_inst():
+    info("instructions-keyboard","Press s to save and quit\
+		Press n to quit without saving\
+		Press c to correct\
+	    	        Press m to show message\
+	    	        Press f to speak\
+	    	        Press e to send email")
+
+info_2 = PushButton(app, command=do_inst, text="Keyboard Instructions", grid=[3,4], align="left")
 
 my_cat = Picture(app, image="giphy.gif") 
