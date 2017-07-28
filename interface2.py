b44c90a61d54fe5806814ffcf2ba2da975f1a12c
JosePedroMachado <30441491+JosePedroMachado@users.noreply.github.com>
2017-07-28 11:33:56 +0100

    Add files via upload
    interface atualizado

--- interface2.py
+++ interface2.py
@@ -77,7 +77,7 @@ def do_booking():
     
 book_seats = PushButton(app, command=do_booking, text="Info", grid=[3,2], align="left") 
 def do_instrucoes():
-    info("instrucões","Press left to (.)\
+    info("instructions-Morse","Press left to (.)\
                             Press rigth to (-)\
                        Press down to add space\
                        Press middle to select a letter\
@@ -88,6 +88,15 @@ def do_instrucoes():
                        Press down for over 2 sec to go back to the menu\
                        Press up for over 2 sec to clean the message")
 
-book_seats = PushButton(app, command=do_instrucoes, text="Instruções", grid=[3,3], align="left")
+info_1 = PushButton(app, command=do_instrucoes, text="Morse Instructions", grid=[3,3], align="left")
+def do_inst():
+    info("instructions-keyboard","Press s to save and quit\
+		Press n to quit without saving\
+		Press c to correct\
+	    	        Press m to show message\
+	    	        Press f to speak\
+	    	        Press e to send email")
+
+info_2 = PushButton(app, command=do_inst, text="Keyboard Instructions", grid=[3,4], align="left")
 
 my_cat = Picture(app, image="giphy.gif") 
